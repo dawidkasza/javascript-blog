e4f9b40c9a55c7057838881d79b0956a94439109
Dawid Kasza <dawidkasza05@gmail.com>
2025-09-22 21:37:43 +0200

    Add function generateTitleLinks

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-'use strict';
+{'use strict';
 
 /*document.getElementById('test-button').addEventListener('click',
 function(){
@@ -41,4 +41,41 @@ const links = document.querySelectorAll('.titles a');
 
 for(let link of links){
    link.addEventListener('click', titleClickHandler);
+}
+
+const optArticleSelector = '.post',
+optTitleSelector = '.post-title',
+optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+   console.log('generate list')
+/* remove contents of titleList */
+   const titleList = document.querySelector(optTitleListSelector);
+   titleList.innerHTML = '';
+
+/* for each article */
+const articles= document.querySelectorAll(optArticleSelector);
+let html = '';
+for(let article of articles){
+   /* get the article id */
+   const articleId= article.getAttribute('id');
+   console.log(articleId);
+   /* find the title element */
+   /* get the title from the title element */
+   const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+   console.log(articleTitle);
+   /* create HTML of the link */
+   const linkHTML = '<li><a href="#'+articleId+'"><span>'+ articleTitle+'</span></a></li>';
+   console.log(linkHTML);
+   /* insert link into titleList */
+   html = html + linkHTML; 
+}
+titleList.innerHTML = html;
+console.log(html);
+}
+
+generateTitleLinks();
+
+
+
 }
